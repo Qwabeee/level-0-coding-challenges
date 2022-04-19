e12625d2ad8f0dd721898ed57a7cf9dd3e8838f0
unknown <nhloniphom397@gmail.com>
2022-04-19 21:09:48 +0200

    task 10 completed

---
+++ task10.js
@@ -0,0 +1,31 @@
+
+     //task 10
+
+     function commonCharacters(str1, str2){
+        let input1 = str1.split("");
+        let input2 = str2.split("");
+        let commonLetters = "";
+    
+        for(let letter in input1){
+            if(input2.indexOf(input1[letter]) > 0 && commonLetters.indexOf(input1[letter]) < 0){   
+                commonLetters != "" ? commonLetters += "," + input1[letter] : commonLetters += input1[letter]; 
+            }
+        }
+
+        for(let letter in input2){
+            if(input1.indexOf(input2[letter]) > 0 && commonLetters.indexOf(input2[letter]) < 0){   
+                commonLetters != "" ? commonLetters += "," + input2[letter] : commonLetters += input2[letter]; 
+            }
+        }
+
+        if(commonLetters == ""){
+            console.log("no matching letters");
+        }
+       if(commonLetters != ""){
+        console.log("Common letters: " + commonLetters)
+       };
+    };
+    
+    
+    
+    
