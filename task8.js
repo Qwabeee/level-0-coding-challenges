de4daea6b4c247a2eea05a6e5d71a11207baaa62
Qwabeee <98186805+Qwabeee@users.noreply.github.com>
2022-04-19 21:30:24 +0200

    Update task8.js

--- task8.js
+++ task8.js
@@ -5,12 +5,18 @@ function convertNumToHour(num){
     let hours = num/60 ;
     let howManyHours = parseInt(hours.toString().split(".")[0]);
     let hourAbbr = howManyHours <= 1 ? "hour" : "hours";
+    let minAbbr;
 
     if( minutes > 0){
         hours = (num - minutes)/60;
     }
     if(hours < 1){
         return num;
+    }
+    if(minutes === 1){
+        minAbbr = "minute";
+    }else{
+        minAbbr = "minutes";
     }
     console.log(hours + hourAbbr, minutes + "minutes")
 
