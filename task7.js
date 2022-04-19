96443d3fb27d33317f02ed9488d5ba474eceeec7
unknown <nhloniphom397@gmail.com>
2022-04-19 20:44:57 +0200

    task 7 completed

---
+++ task7.js
@@ -0,0 +1,13 @@
+
+//task 7
+
+function fahrenheitTemp(temp){
+    fahrenheitTemperature = temp * 9.0 / 5 + 32;
+    return fahrenheitTemperature;
+    
+}
+
+function celsiusTemp(temp){
+    celsiusTemperature = 5/9 * (temp - 32) ;
+    return celsiusTemperature;
+}
