718d1e092773a477f9a51c43d31351a9633894e2
Qwabeee <98186805+Qwabeee@users.noreply.github.com>
2022-04-20 12:52:08 +0200

    updated task 2

--- task2.js
+++ task2.js
@@ -1,11 +1,11 @@
 
 //task 2
 
-x = 1 + 1 * 2;
-y = (1 + 1) * 2;
-let z = 1 + ( 1 * 2 );
-let a = 1 + 1 * 2 / 2;
-let b = (1 + 1 * 2 ) /  2;
+const x = 1 + 1 * 2;
+const y = (1 + 1) * 2;
+const z = 1 + ( 1 * 2 );
+const a = 1 + 1 * 2 / 2;
+const b = (1 + 1 * 2 ) /  2;
 
 console.log(x);
 console.log(y);
