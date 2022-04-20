9996165077e1f9a052ea2211ac7349262aa19bff
Qwabeee <98186805+Qwabeee@users.noreply.github.com>
2022-04-20 13:02:49 +0200

    updated task 5

--- task5.js
+++ task5.js
@@ -1,11 +1,11 @@
 
 //task 5
 
-function triangleArea(a, b, c){
-    let s = (1/2) * (a + b + c);
+function calculateTriangleAreaUsingSidesLength(side1, side2, side3){
+    const semiPerimeter = (1/2) * (side1 + side2 + side3);
     let area = 0;
-    if( s > 0){
-        area = Math.sqrt((s)*(s-a)*(s-b)*(s-c)); 
+    if( semiPerimeter > 0){
+        area = Math.sqrt((semiPerimeter)*(semiPerimeter - side1)*(semiPerimeter - side2)*(semiPerimeter - side3)); 
     }else{
         area = undefined;
     }
