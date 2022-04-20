212983d7d0fbc8cb721f705a9e344212f45ae524
Qwabeee <98186805+Qwabeee@users.noreply.github.com>
2022-04-20 18:28:59 +0200

    Update task9.js

--- task9.js
+++ task9.js
@@ -2,8 +2,17 @@
 //task 9
 
 function printVowels(str){
-    let input = str;
-    let vowels = /[aeiou]/gi;
-    let result = input.match(vowels);
-    console.log("Vowels: ", result);
+    const input = str;
+    const vowels = /[aeiou]/gi;
+    const result = input.match(vowels);
+    let output = [];
+
+    for(let i = 0; i < result.length; i++){
+        if( output.indexOf(result[i]) === -1){
+            output.push(result[i]);
+            
+   }
+  
+  };
+  console.log(`Vowels: ${output.join(",")}`);
 }
