4c1b78f67d1c8d562fab8a91469f9040ae19128f
Qwabeee <98186805+Qwabeee@users.noreply.github.com>
2022-04-20 13:12:55 +0200

    updated task 7

--- task7.js
+++ task7.js
@@ -1,13 +1,13 @@
 
 //task 7
 
-function fahrenheitTemp(temp){
-    fahrenheitTemperature = temp * 9.0 / 5 + 32;
+function convertCelsiusTempToFahrenheitTemp(temp){
+    const fahrenheitTemperature = temp * 9.0 / 5 + 32;
     return fahrenheitTemperature;
     
 }
 
-function celsiusTemp(temp){
-    celsiusTemperature = 5/9 * (temp - 32) ;
+function convertFahrenheitTempToCelsiusTemp(temp){
+    const celsiusTemperature = 5/9 * (temp - 32) ;
     return celsiusTemperature;
 }
