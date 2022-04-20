6e9e7ca503efc82242775fca54697c111add689c
Qwabeee <98186805+Qwabeee@users.noreply.github.com>
2022-04-20 13:20:46 +0200

    updated task 8

--- task8.js
+++ task8.js
@@ -1,10 +1,10 @@
 //task 8
 
 function convertNumToHour(num){
-    let minutes = num % 60;
+    const minutes = num % 60;
     let hours = num/60 ;
     let howManyHours = parseInt(hours.toString().split(".")[0]);
-    let hourAbbr = howManyHours <= 1 ? "hour" : "hours";
+    const hourAbbr = howManyHours <= 1 ? "hour" : "hours";
     let minAbbr;
 
     if( minutes > 0){
@@ -13,12 +13,12 @@ function convertNumToHour(num){
     if(hours < 1){
         return num;
     }
-    if(minutes === 1){
+    if( minutes === 1){
         minAbbr = "minute";
     }else{
         minAbbr = "minutes";
     }
-    console.log(hours + hourAbbr, minutes + "minutes")
+    console.log(hours + hourAbbr, minutes + minAbbr)
 
-}
+};
 
